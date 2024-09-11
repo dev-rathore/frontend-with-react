beecaac6eb3e5ebc8127a0f5113b5df0123c2de1
dev-rathore <rathoredvd3001@gmail.com>
2024-09-11 22:33:11 +0530

    initial commit

---
+++ src/concepts/essentials/introduction.js
@@ -0,0 +1,5 @@
+/* createRoot of ReactDOM (From react-dom/client) creates a root element, createRoot returns an Root Object 
+which contains render method that can be used to render components inside root element */
+
+/* createElement (From react) takes three props (element name, attributes (e.g. style, onClick, etc), and 
+children (Either string or ReactNode[])) and create component out of it. */
